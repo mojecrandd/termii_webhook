52ee958762a67194f2a6f91de7a2d6c8fedefbfd
tobi-oye <tobiloba5000@gmail.com>
2022-02-16 20:00:28 +0100

    [feat]: remove comments and add console

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,16 @@
 var express = require('express');
 const bodyParser = require('body-parser');
-
-// var router = express.Router();
 const router = express()
 router.use(express.json());
-// app.use(express.urlencoded({ extended: true }));
 router.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser);
-// app.use(bodyParser.urlencoded());
 router.use(bodyParser.json());
 /* GET home page. */
 router.post('/', function(req, res, next) {
   // res.send({ title: 'Express' })
   // console.log(res.json({requestBody: req.body}))
-  // console.log(req.body)
-  res.json({requestBody: req.body})
+  console.log(req.body)
+  res.json({status: "ok"})
+  res.end();
 
 });
 
